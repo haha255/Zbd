6fd2e98448fcd5dd9efcbf8f435526d85c83eb0f
haha255 <450558904@qq.com>
2020-01-12 22:27:25 +0800

    添加了自制的中间件。

--- zbd_site/settings.py
+++ zbd_site/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.system.middleware.MenuCollection',
 ]
 
 ROOT_URLCONF = 'zbd_site.urls'
